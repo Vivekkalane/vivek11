1510b07a04ba84a8ee451cb766b7d9b4469e0e06
Vivekkalane <125533578+Vivekkalane@users.noreply.github.com>
2023-02-17 07:48:22 +0000

    added new codes blocks

--- main.tf
+++ main.tf
@@ -1,13 +1,34 @@
 provider "azurerm" {
     features {}
 }
+variable "location" {
+  default = "eastus"
+}
+variable "name" {
+  default = "Vivek_Cosmos"
+}
 resource "azurerm_resource_group" "Vivek_Cosmos" {
-  location = "eastus"
-  name = "Vivek_DB1"
+  location = var.location
+  name = var.name
   tags = {
     Name = "Vivek"
     Day = "6"
-    Team = "DataApps"
-    Role = "Data Engineer"
+  }
+}
+resource "azurerm_cosmosdb_account" "GlobalDocumentDB" {
+  name                      = var.name
+  location                  = var.location
+  resource_group_name       = azurerm_resource_group.Vivek_Cosmos.name
+  offer_type                = "Standard"
+  kind                      = "GlobalDocumentDB"
+  enable_automatic_failover = false
+  geo_location {
+    location          = var.location
+    failover_priority = 0
+  }
+  consistency_policy {
+    consistency_level       = "BoundedStaleness"
+    max_interval_in_seconds = 300
+    max_staleness_prefix    = 100000
   }
 }
